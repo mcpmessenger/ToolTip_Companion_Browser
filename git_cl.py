6da6aecbb4213d99d41ff67eac13c2d1e23e23c9
Jiewei Qian <qjw@chromium.org>
2025-09-16 17:00:01 -0700

    git-cl: make SetReview ReAuth capable
    This CL changes SetReview to be ReAuth capable.
    
    This is a potential breaking change if an downstream script relies on
    gerrit_util.SetReview but hasn't implemented ReAuth.
    
    If this caused breakage, please refer to https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/gerrit_reauth.md#Troubleshooting for troubleshooting instructions.
    
    If the above doesn't work, please report a bug, then add `LUCI_BYPASS_REAUTH=1` to the environment to disable ReAuth for now.
    
    Bug: 442666611
    Change-Id: I7724f15f166f9975fc88be5d8e1c93be4e1ec302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/6939308
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Scott Lee <ddoman@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>

--- git_cl.py
+++ git_cl.py
@@ -2270,7 +2270,8 @@ class Changelist(object):
             gerrit_util.SetReview(self.GetGerritHost(),
                                   self._GerritChangeIdentifier(),
                                   labels=labels,
-                                  notify=notify)
+                                  notify=notify,
+                                  project=self.GetGerritProject())
             return 0
         except KeyboardInterrupt:
             raise
@@ -2588,7 +2589,8 @@ class Changelist(object):
         gerrit_util.SetReview(self.GetGerritHost(),
                               self._GerritChangeIdentifier(),
                               msg=message,
-                              ready=publish)
+                              ready=publish,
+                              project=self.GetGerritProject())
 
     def GetCommentsSummary(self, readable=True):
         # DETAILED_ACCOUNTS is to get emails in accounts.
